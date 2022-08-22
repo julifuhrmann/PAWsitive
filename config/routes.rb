4a274f797c25231fc982a6c5ae9401efa9423aa6
Tahir Günsen <tgunsen@pm.me>
2022-08-22 15:19:35 +0200

    created CastsController

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'cats/index'
+  get 'cats/show'
+  get 'cats/new'
+  get 'cats/create'
+  get 'cats/edit'
+  get 'cats/update'
+  get 'cats/destroy'
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
