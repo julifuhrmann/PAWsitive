f5c23c6588a2a94861a9adac61ca62b1fa2aa1e0
Juliana Fuhrmann <julifuhrmann@hotmail.com>
2022-08-22 16:36:52 +0200

    controllers for cats and bookings

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,17 @@
+class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
+end
--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,22 +1,41 @@
 class CatsController < ApplicationController
   def index
+    @cats = Cat.all
   end
 
   def show
+    @cat = Cat.find(params[:id])
   end
 
   def new
+    @cat = Cat.new
   end
 
   def create
+    @cat = Cat.new(cat_params)
+    @cat.save
+    redirect_to cats_path(@cat)
   end
 
   def edit
+    @cat = Cat.find(params[:id])
   end
 
   def update
+    @cat = Cat.find(params[:id])
+    @cat.update(cat_params)
+    redirect_to cat_path(@cat)
   end
 
   def destroy
+    @cat = Cat.find(params[:id])
+    @cat.destroy
+    redirect_to cats_path, status: :see_other
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :age, :description, :status)
   end
 end
