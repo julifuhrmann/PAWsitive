af30901b4bec1bbf6d2ddf64c827c16a8c7ad711
Juliana Fuhrmann <julifuhrmann@hotmail.com>
2022-08-25 15:14:16 +0200

    search bar working

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -2,4 +2,11 @@ class Cat < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   has_many :bookings, dependent: :destroy
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_age,
+    against: [ :name, :age ],
+    using: {
+    tsearch: { prefix: true }
+  }
 end
